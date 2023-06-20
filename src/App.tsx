197ce39f70440dc02d1f9ab5861fc707dadcef99
Mrleoneed <mrleoneed@gmail.com>
2023-06-20 15:32:50 +0300

    01 props

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
-    return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
-}
 
+    const task1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+    ]
+
+    const task2 = [
+        {id: 1, title: 'Lager', isDone: false},
+        {id: 2, title: 'IPA', isDone: true},
+        {id: 3, title: 'Stout', isDone: false},
+    ]
+
+    return (
+        <div className={'App'}>
+            <Todolist title={"What to learn 1"} tasks={task1}/>
+            <Todolist title={"Beer"} tasks={task2}/>
+
+        </div>
+    )
+        ;
+}
 
 
 export default App;
