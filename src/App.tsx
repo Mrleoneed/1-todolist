e30cf39d04d0c38c6dc66225579e2d5f36142b7f
Mrleoneed <mrleoneed@gmail.com>
2023-06-27 21:17:54 +0300

    02 delete/filer task

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {TaskType, Todolist} from "./Todolist";
+import {FilterType, Todolist} from './Todolist';
 
 function App() {
-
-    const task1: TaskType[] = [
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false},
-    ]
-
-    const task2: TaskType[] = [
-        {id: 1, title: 'Lager', isDone: false},
-        {id: 2, title: 'IPA', isDone: true},
-        {id: 3, title: 'Stout', isDone: false},
-    ]
-
-    return (
-        <div className={'App'}>
-            <Todolist title={"What to learn 1"} tasks={task1}/>
-            <Todolist title={"Beer"} tasks={task2}/>
-
-        </div>
+    let [tasks, setTasks] = useState([
+            {id: 1, title: "HTML&CSS", isDone: true},
+            {id: 2, title: "JS", isDone: true},
+            {id: 3, title: "ReactJS", isDone: false},
+            {id: 4, title: "Redux", isDone: false}
+        ]
     )
-        ;
+
+    const removeTask = (taskId: number) => {
+        setTasks(tasks.filter(el => el.id !== taskId))
+    }
+
+
+    // const [valueFilter, setValueFilter] = useState<FilterType>('All')
+    // const taskFilter = (value: FilterType) => {
+    //     setValueFilter(value)
+    // }
+    // if (valueFilter === 'Active') {
+    //     tasks = tasks.filter(el => !el.isDone)
+    // }
+    // if (valueFilter === 'Completed') {
+    //     tasks = tasks.filter(el => el.isDone)
+    // }
+    return (
+        <div className="App">
+            <Todolist
+                title="What to learn"
+                tasks={tasks}
+                removeTask={removeTask}
+                // taskFilter={taskFilter}
+            />
+        </div>
+    );
 }
 
-
 export default App;
-
-
-
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,39 +1,75 @@
-import React from "react";
+import React, {useState} from 'react';
 
-type PropsType = {
-    title: string
-    tasks: TaskType[]
-}
-
-export type TaskType = {
+type TaskType = {
     id: number
     title: string
     isDone: boolean
 }
 
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (taskId: number) => void
+    // taskFilter: (value: FilterType) => void
 
-export const Todolist = (props: PropsType) => {
-    return (
+}
+
+export type FilterType = 'All' | 'Active' | 'Completed'
+
+
+export function Todolist(props: PropsType) {
+
+    const [valueFilter, setValueFilter] = useState<FilterType>('All')
+
+    let taskFilter = (value: FilterType) => {
+        setValueFilter(value)
+    }
+    const durshlagFoo = () => {
+        let durshlag = props.tasks
+        //     if (valueFilter === 'Active') {
+        //         durshlag = props.tasks.filter(el => !el.isDone)
+        //     }
+        //     if (valueFilter === 'Completed') {
+        //         durshlag = props.tasks.filter(el => el.isDone)
+        //     }
+        //     return durshlag
+        // }
+        switch (valueFilter) {
+            case "Active": {
+                return durshlag = props.tasks.filter(el => !el.isDone)
+            }
+            case "Completed": {
+                return durshlag = props.tasks.filter(el => el.isDone)
+            }
+            default:
+                return durshlag
+
+        }
+    }
+
+
+    return <div>
+        <h3>{props.title}</h3>
         <div>
-            <h3>{props.title}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                {props.tasks.map((el) => {
-                    return (
-                        <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
-                    )
-                })}
-
-
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
+            <input/>
+            <button>+</button>
         </div>
-    )
+        <ul>
+            {durshlagFoo().map(t => {
+                return (
+                    <li>
+                        <button onClick={() => props.removeTask(t.id)}> &#10060;</button>
+                        <input type="checkbox" checked={t.isDone}/>
+                        <span>{t.title}</span>
+
+                    </li>
+                )
+            })}
+        </ul>
+        <div>
+            <button onClick={() => taskFilter('All')}>All</button>
+            <button onClick={() => taskFilter('Active')}>Active</button>
+            <button onClick={() => taskFilter('Completed')}>Completed</button>
+        </div>
+    </div>
 }
